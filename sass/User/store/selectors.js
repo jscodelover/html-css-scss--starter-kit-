2b87e3a053b81b34aaea76b60d7277baf0179178
manisha1896 <manisha.4918@gmail.com>
2018-11-18 18:50:20 +0530

    included grid layout

--- sass/User/store/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { isEmpty } from 'lodash';
-
-const userStateSelector = (state) => state.user;
-
-export const isLoggedInSelector = createSelector(
-  userStateSelector,
-  ({ user }) => !isEmpty(user),
-);
